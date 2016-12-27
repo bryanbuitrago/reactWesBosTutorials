82214387c154a2b5768fcae78bdfaeb3e3fd7088
bryan buitrago <bryanbuitrago86@gmail.com>
2016-12-27 16:07:44 -0500

    Passing Dynamic data with props. Video # 7

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
+    console.log(this);
     return (
-       <p>Header</p>
+       <header className = "top">
+        <h1>
+          Catch
+          <span className = "ofThe">
+          <span className = "ofThe">of</span>
+          <span className = "ofThe">the</span>
+          </span>
+          Day
+          </h1>
+        <h3 className = "tagline">{this.props.tagline}</h3> {/* With Variables we use this.props.<property>*/}
+        {/*<h3 className = "tagline">Fill Me In</h3> // Without variables*/}
+        </header>
       )
   }
 }
