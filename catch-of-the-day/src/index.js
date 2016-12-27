86b455aec3402c45f5621ecf3305a741c922e7e0
bryan buitrago <bryanbuitrago86@gmail.com>
2016-12-26 20:55:19 -0500

    Video 3 REACT WesBos code along

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,18 @@
 // let's go!
+import React from 'react'; // imports react from react
+import { render } from 'react-dom'; //Imports the render method from the DOM package
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main')); // renders the Component
+
+// // 1 page way of Components
+// // Component begins
+// // Every Component needs at least a method to be a Component I.E. render() on line 8
+// class StorePicker extends React.Component {
+//   render(){
+//     return <p>Hello World</p>
+//   }
+// }
+// //
+// render(<StorePicker/>, document.querySelector('#main')); //renders the Component
