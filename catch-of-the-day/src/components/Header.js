e9f31cfc0c97f34df9b985635c1b908eca0dc267
bryan buitrago <bryanbuitrago86@gmail.com>
2016-12-27 16:18:13 -0500

    Stateless Functional Components. Video # 8

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    console.log(this);
+// Stateless Functional Component. Video #8.
+  const Header = () => {
     return (
        <header className = "top">
         <h1>
@@ -13,11 +12,32 @@ class Header extends React.Component {
           </span>
           Day
           </h1>
-        <h3 className = "tagline">{this.props.tagline}</h3> {/* With Variables we use this.props.<property>*/}
+        <h3 className = "tagline">{props.tagline}</h3> {/* With Variables we use props.<property>*/}
         {/*<h3 className = "tagline">Fill Me In</h3> // Without variables*/}
         </header>
       )
   }
-}
 
-export default Header; // Exports the Header Component
+
+// The code below was covered in video 7.
+// class Header extends React.Component {
+//   render() {
+//     console.log(this);
+//     return (
+//        <header className = "top">
+//         <h1>
+//           Catch
+//           <span className = "ofThe">
+//           <span className = "ofThe">of</span>
+//           <span className = "ofThe">the</span>
+//           </span>
+//           Day
+//           </h1>
+//         <h3 className = "tagline">{this.props.tagline}</h3> {/* With Variables we use this.props.<property>*/}
+//         {/*<h3 className = "tagline">Fill Me In</h3> // Without variables*/}
+//         </header>
+//       )
+//   }
+// }
+
+// export default Header; // Exports the Header Component
