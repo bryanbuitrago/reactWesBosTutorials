9073e4d605eb6d5ef592aa4442a3d98e207c9d76
bryan buitrago <bryanbuitrago86@gmail.com>
2016-12-28 18:54:04 -0500

    All about React Router Video # 12

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
+
+  goToStore() {
+    event.preventDefault();
+    console.log('You changed the URL');
+    // First grab the text from the box
+    const storeId = this.storeInput.value
+    // Second transition from / to /store/:storeId
+    this.context.router.transitionTo('store/${storeId}');
+  }
   render(){
     // JSX allows us to write html right inside Javascript. Community Standard
     return (
       // Here we can write all our html
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
       <h2> Please Enter A Store </h2>
-      <input type="text" required placeholder="Store Name"/>
+      <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
       <button type="submit"> Visit Store </button>
       </form>
       )
@@ -19,4 +28,7 @@ class StorePicker extends React.Component {
   }
 }
 
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
 export default StorePicker; //exports the StorePicker Component
